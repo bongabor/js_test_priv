d49ecda3b729eb2cc30fa69036562e2e72c117ac
Bonyai, Gabor <bongabor@gmail.com>
2021-09-23 11:33:16 +0200

    Adding webpack dev server instead of http-server

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery'
+$(document).ready(()=> {
+    console.log('hello from jquery');
+   });
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document Loaded');
+});
