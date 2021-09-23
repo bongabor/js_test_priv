32637e51c393ed2b1636193c8828d1665a7dd2a7
Bonyai, Gabor <bongabor@gmail.com>
2021-09-23 12:28:44 +0200

    Including the CSS Extract plugin to the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     module:{
-      rules:[{
-          test:/\.js$/,
-          exclude: /node_modules/,
-          use:{
-               loader:"babel-loader"
-          }
-      },
-      {
-        test:/\.html$/,
-        use:{
-             loader: "html-loader",
-              options: {minimize:true}
-      }
-      }],
+        rules:[
+            { test:/\.js$/, exclude: /node_modules/, use: "babel-loader"},
+            { test:/\.html$/, use:{
+                loader: "html-loader",
+                options: {minimize:true}
+            }},
+            { test:/\/scss$/, use: [
+                {loader: "style-loader"},
+                {loader:  "css-loader"},
+                {loader:  "sass-loader"} 
+            ]},
+        ]
     },
     plugins:[
-    new HtmlWebpackPlugin({
-        template:"./index.html",
-        filename: "./index.html"
+        new HtmlWebpackPlugin({
+            template:"./index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkfile: "[id].css"
         })
     ],
 };
